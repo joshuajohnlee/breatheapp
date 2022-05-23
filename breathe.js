fe192a61f72069eb9d7d7c5f46457fbbd3579f5e
JoystickJoshy <joshuajohnlee@me.com>
2022-05-23 22:33:23 +0100

    audio and its title now dynamically added to js array

--- breathe.js
+++ breathe.js
@@ -1,8 +1,7 @@
-    // Initialise variables for controls
-    let speed = 5;
-    let playervolume = 5;
-    let currentaudiotrack = 0;
-
+// Initialise variables for controls
+let speed = 5;
+let playervolume = 5;
+let currentaudiotrack = 0;
 
 // Set variables for HTML elements
 let speedreadout = document.getElementById('speedreadout');
@@ -15,12 +14,31 @@ let audioplaybutton = document.getElementById('audioplaybutton');
 let nowplaying = document.getElementById('nowplaying');
 let audioskipforward = document.getElementById('audioskipforward');
 let audioskipback = document.getElementById('audioskipback');
-let audio1 = document.getElementById('audio1');
-let audio2 = document.getElementById('audio2');
-let audio3 = document.getElementById('audio3');
+
+// Populate audio elements
+// This checks for any element named audio with a number, and adds it to the audios array.
+// The audios array is used later to pause and play the correct track
+
+let audios = []
+let audiolisting = true;
+let counter = 0;
+
+while (audiolisting) {
+    // Set a string equal to the name of the next audio element to check for
+    string = 'audio' + (counter+1).toString();
+
+    if (document.getElementById(string) != null) { 
+    audios[counter] = [document.getElementById(string), document.getElementById(string).getAttribute("data-title")];
+    counter++;
+    } else {
+        audiolisting = false;
+    }
+}
+
+console.log("Audio listing complete, found " + audios.length + " items.")
 
 // Create an array of audio elements
-const audios = [audio1, audio2, audio3];
+// const audios = [audio1, audio2, audio3];
 
 // Add button listeners
 speedupbutton.addEventListener("click", speedchangeup);
@@ -66,57 +84,47 @@ function volumechangedown() {
     if (playervolume > 1 && playervolume <= 10) {
         playervolume--
     }
-    audios[currentaudiotrack].volume = playervolume/10
+    audios[currentaudiotrack][0].volume = playervolume/10
     volumereadout.innerHTML = "VOLUME:<br>" + playervolume * 10 + "%"
 }
 
 function playbacktoggle() {
-    if (audios[currentaudiotrack].paused) {
-        audios[currentaudiotrack].play()
+    if (audios[currentaudiotrack][0].paused) {
+        audios[currentaudiotrack][0].play()
         audioplaybutton.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
         updateTitle();
-    } else if (!audios[currentaudiotrack].paused) {
-        audios[currentaudiotrack].pause()
+    } else if (!audios[currentaudiotrack][0].paused) {
+        audios[currentaudiotrack][0].pause()
         audioplaybutton.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>'
         nowplaying.innerHTML = "PAUSED";
     }
 }
 
 function skipforward() {
-    audios[currentaudiotrack].pause();
+    audios[currentaudiotrack][0].pause();
     if (currentaudiotrack < audios.length - 1) {
         currentaudiotrack++
-        audios[currentaudiotrack].play()
+        audios[currentaudiotrack][0].play()
     } else {
         currentaudiotrack = 0;
-        audios[currentaudiotrack].play()
+        audios[currentaudiotrack][0].play()
     }
     updateTitle();
 }
 
 function skipback() {
-    audios[currentaudiotrack].pause();
+    audios[currentaudiotrack][0].pause();
     if (currentaudiotrack > 0) {
         currentaudiotrack--
-        audios[currentaudiotrack].play()
+        audios[currentaudiotrack][0].play()
     } else {
         currentaudiotrack = audios.length - 1;
-        audios[currentaudiotrack].play()
+        audios[currentaudiotrack][0].play()
     }
     updateTitle();
 }
 
 // Could replace this with something more dynamic - have audio elements and their titles in a multilayer array?
 function updateTitle() {
-    switch(currentaudiotrack) {
-        case 0:
-            nowplaying.innerHTML = "Now playing: Relaxing music"
-            break;
-        case 1:
-            nowplaying.innerHTML = "Now playing: Rain Sounds"
-            break;
-        case 2:
-            nowplaying.innerHTML = "Now playing: Binaural Beats"
-            break;
-    }
+    nowplaying.innerHTML= "Now playing: " + audios[currentaudiotrack][1];
 }
