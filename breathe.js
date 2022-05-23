925a1c6413f6d7df9fd297662071f3a4ca4a2638
JoystickJoshy <joshuajohnlee@me.com>
2022-05-23 21:05:38 +0100

    complete html audio player, added JS for audio player, moved CSS to assets folder.

--- breathe.js
+++ breathe.js
@@ -1,33 +1,40 @@
+    // Initialise variables for controls
+    let speed = 5;
+    let playervolume = 5;
+    let currentaudiotrack = 0;
+
+
 // Set variables for HTML elements
-const speedreadout = document.getElementById('speedreadout');
-const speedupbutton = document.getElementById('speedup');
-const speeddownbutton = document.getElementById('speeddown')
+let speedreadout = document.getElementById('speedreadout');
+let speedupbutton = document.getElementById('speedup');
+let speeddownbutton = document.getElementById('speeddown')
+let volumereadout = document.getElementById('volumereadout');
+let volumeupbutton = document.getElementById('volumeup');
+let volumedownbutton = document.getElementById('volumedown');
+let audioplaybutton = document.getElementById('audioplaybutton');
+let nowplaying = document.getElementById('nowplaying');
+let audioskipforward = document.getElementById('audioskipforward');
+let audioskipback = document.getElementById('audioskipback');
+let audio1 = document.getElementById('audio1');
+let audio2 = document.getElementById('audio2');
+let audio3 = document.getElementById('audio3');
 
-const volumereadout = document.getElementById('volumereadout');
-const volumeupbutton = document.getElementById('volumeup');
-const volumedownbutton = document.getElementById('volumedown')
-
-const audioplaybutton = document.getElementById('audioplay')
-
-const music = document.getElementById('music1')
+// Create an array of audio elements
+const audios = [audio1, audio2, audio3];
 
 // Add button listeners
-
 speedupbutton.addEventListener("click", speedchangeup);
 speeddownbutton.addEventListener("click", speedchangedown);
 volumeupbutton.addEventListener("click", volumechangeup);
 volumedownbutton.addEventListener("click", volumechangedown);
 audioplaybutton.addEventListener("click", playbacktoggle);
-
-// Initialise variables for controls 
-
-let speed = 5;
-let playervolume = 5;
+audioskipforward.addEventListener("click", skipforward);
+audioskipback.addEventListener("click", skipback);
 
 // Write out the intial values
-
 speedreadout.innerHTML = speed;
 volumereadout.innerHTML = playervolume * 10 + "%";
+
 
 // Speed change functions
 
@@ -51,7 +58,7 @@ function volumechangeup() {
     if (playervolume >= 1 && playervolume < 10) {
         playervolume++
     } 
-    music.volume = playervolume/10
+    audios[currentaudiotrack].volume = playervolume/10
     volumereadout.innerHTML = playervolume * 10 + "%"
 }
     
@@ -59,17 +66,57 @@ function volumechangedown() {
     if (playervolume > 1 && playervolume <= 10) {
         playervolume--
     }
-    music.volume = playervolume/10
+    audios[currentaudiotrack].volume = playervolume/10
     volumereadout.innerHTML = playervolume * 10 + "%"
 }
 
 function playbacktoggle() {
-    if (music.paused) {
-        music.play()
-        audioplaybutton.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>'
-    } else if (!music.paused) {
-        music.pause()
+    if (audios[currentaudiotrack].paused) {
+        audios[currentaudiotrack].play()
+        audioplaybutton.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
+        updateTitle();
+    } else if (!audios[currentaudiotrack].paused) {
+        audios[currentaudiotrack].pause()
         audioplaybutton.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>'
+        nowplaying.innerHTML = "PAUSED";
     }
 }
 
+function skipforward() {
+    audios[currentaudiotrack].pause();
+    if (currentaudiotrack < audios.length - 1) {
+        currentaudiotrack++
+        audios[currentaudiotrack].play()
+    } else {
+        currentaudiotrack = 0;
+        audios[currentaudiotrack].play()
+    }
+    updateTitle();
+}
+
+function skipback() {
+    audios[currentaudiotrack].pause();
+    if (currentaudiotrack > 0) {
+        currentaudiotrack--
+        audios[currentaudiotrack].play()
+    } else {
+        currentaudiotrack = audios.length - 1;
+        audios[currentaudiotrack].play()
+    }
+    updateTitle();
+}
+
+// Could replace this with something more dynamic - have audio elements and their titles in a multilayer array?
+function updateTitle() {
+    switch(currentaudiotrack) {
+        case 0:
+            nowplaying.innerHTML = "Now playing: Relaxing music"
+            break;
+        case 1:
+            nowplaying.innerHTML = "Now playing: Rain Sounds"
+            break;
+        case 2:
+            nowplaying.innerHTML = "Now playing: Binaural Beats"
+            break;
+    }
+}
